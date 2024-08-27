4b91dca3ee978044b1aec34662bbcae62b135156
macgonzales94 <ma.cgonzales.94@gmail.com>
2024-08-26 23:43:05 -0500

    modulo4

--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,get_list_or_404
+from django.shortcuts import render,get_list_or_404,redirect
 
-from . models import Categoria,Producto
+from . models import Categoria,Producto,Cliente
 # Create your views here.
 """"VISTAS PARA EL CATALOGO DE PRODUCTOS"""
 def index(request):
@@ -51,3 +51,171 @@ def productoDetalle(request,producto_id):
     }
 
     return render(request,'producto.html',context)
+
+""" VISTAS PARA EL CARRITO DE COMPRAS"""
+
+from .carrito import Cart
+
+def carrito(request):
+    return render(request,'carrito.html')
+
+def agregarCarrito(request,producto_id):
+
+    if request.method == "POST":
+        cantidad = int(request.POST["cantidad"])
+    else:
+        cantidad = 1
+        
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto,cantidad)
+
+    if request.method == 'GET':
+        return redirect('/')
+    
+    return render(request,'carrito.html')
+
+
+def eliminarProductoCarrito(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+
+    return render(request, "carrito.html")
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+
+    return render(request, 'carrito.html')
+
+"""VISTAS PARA CLIENTES Y USUARIOS"""
+
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+from django.urls import resolve, Resolver404
+
+from .forms import ClienteForm
+
+def crearUsuario(request):
+
+    if request.method == 'POST':
+        dataUsuario = request.POST['nuevoUsuario']
+        dataPassword = request.POST['nuevoPassword']
+
+        nuevoUsuario = User.objects.create_user(username=dataUsuario,password=dataPassword)
+        if nuevoUsuario is not None:
+            login(request,nuevoUsuario)
+            return redirect('/cuenta')
+
+    return render(request,'login.html')
+
+
+def cuentaUsuario(request):
+
+    try:
+
+        clienteEditar = Cliente.objects.get(usuario = request.user)
+        dataCliente = {
+                'nombres' : request.user.first_name,
+                'apellidos' : request.user.last_name,
+                'email': request.user.email,
+                'direccion' : clienteEditar.direccion,
+                'telefono' : clienteEditar.telefono,
+                'dni' : clienteEditar.dni,
+                'sexo' : clienteEditar.sexo,
+                'fecha_nacimiento' : clienteEditar.fecha_nacimiento
+            }
+    except:
+        dataCliente = {
+                'nombres' : request.user.first_name,
+                'apellidos' : request.user.last_name,
+                'email': request.user.email,
+            }
+        
+    frmCliente = ClienteForm(dataCliente)
+    context = {
+        'frmCliente':frmCliente
+    }
+    return render(request,'cuenta.html',context)
+
+def logoutUsuario(request):
+    logout(request)
+    return render(request,'login.html')
+    
+def loginUsuario(request):
+    paginaDestino = request.GET.get('next', None)
+    context = {
+        'destino': paginaDestino
+    }
+
+    if request.method == 'POST':
+        dataUsuario = request.POST['usuario']
+        dataPassword = request.POST['password']
+        dataDestino = request.POST.get('destino', '')
+
+        usuarioAuth = authenticate(request, username=dataUsuario, password=dataPassword)
+        if usuarioAuth is not None:
+            login(request, usuarioAuth)
+            if dataDestino:
+                try:
+                    # Verifica si la URL es resolvible
+                    resolve(dataDestino)
+                    return redirect(dataDestino)
+                except Resolver404:
+                    pass  # Si no se puede resolver, continua a /cuenta
+
+            return redirect('/cuenta')
+        else:
+            context = {
+                'mensajeError': 'Datos Incorrectos'
+            }
+
+    return render(request, 'login.html', context)
+
+
+def actualizarCliente(request):
+    
+    if request.method == 'POST':
+        frmCliente = ClienteForm(request.POST)
+        if frmCliente.is_valid():
+            dataCliente = frmCliente.cleaned_data
+
+            #actualizar usuario
+            actUsuario = User.objects.get(pk=request.user.id)
+            actUsuario.first_name = dataCliente["nombres"]
+            actUsuario.last_name = dataCliente["apellidos"]
+            actUsuario.email = dataCliente["email"]
+            actUsuario.save()
+
+            #registrar cliente
+            nuevoCliente = Cliente()
+            nuevoCliente.usuario = actUsuario
+            nuevoCliente.dni = dataCliente["dni"]
+            nuevoCliente.direccion = dataCliente["direccion"]
+            nuevoCliente.telefono = dataCliente["telefono"]
+            nuevoCliente.sexo = dataCliente["sexo"]
+            nuevoCliente.fecha_nacimiento = dataCliente["fecha_nacimiento"]
+            nuevoCliente.save()
+
+    mensaje = "Datos Actualizados"
+
+    context ={
+        'mensaje':mensaje,
+        'frmCliente':frmCliente
+    }
+
+
+    return render(request,'cuenta.html',context)
+
+""" VISTAS PARA PROCESO DE COMPRA"""
+@login_required(login_url='/login')
+def registrarPedido(request):
+    frmCliente = ClienteForm()
+    context = {
+
+        'frmCliente':frmCliente
+    }
+
+    return render(request,'pedido.html',context)
